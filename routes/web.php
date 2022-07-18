0de3dd3029554ef453a007eb4076631f9d74dc23
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-18 15:46:35 +0200

    creazione front.js e .scss per vue

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-
+// Back Office
 Route::middleware('auth')
 ->namespace('Admin') //tutti i controller saranno dentro al namespace Admin
 ->name('admin.') //nome della rotta
@@ -28,6 +28,7 @@ Route::middleware('auth')
     Route::get('/home', 'HomeController@index')->name('home');
 });
 
+// Front Office
 Route::any('{any?}', function() {
     return view('guest.home');
 })->where('any', '.*');
