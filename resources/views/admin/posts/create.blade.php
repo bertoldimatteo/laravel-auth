24531299d6e0ad4c1d837b540da24b26697f5353
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-19 17:05:48 +0200

    continuo lavoro su create.blade

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Creazione post</h1>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h1>Creazione post</h1>
+            </div>
+            <div class="card-body"></div>
+        </div>
+    </div>
 @endsection
 
