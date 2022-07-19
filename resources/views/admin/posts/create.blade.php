f99b83fe292bf7c2db9b7af4a3f7ff44d1edd1f8
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-19 17:26:30 +0200

    form terminato

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,7 +6,24 @@
             <div class="card-header">
                 <h1>Creazione post</h1>
             </div>
-            <div class="card-body"></div>
+            <div class="card-body">
+                <form action="{{route('admin.posts.store')}}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="title">Titolo</label>
+                        <input type="text" class="form-control" id="title" name="title">
+                    </div>
+                    <div class="form-group">
+                        <label for="title">Post</label>
+                        <textarea name="content" id="content" class="form-control" cols="30" rows="5"></textarea>
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="published" name="published">
+                        <label class="form-check-label" for="published">Pubblica</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Crea</button>
+                  </form>
+            </div>
         </div>
     </div>
 @endsection
