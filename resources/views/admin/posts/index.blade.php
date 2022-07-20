bee41edc9348c6688d9b3e203dd1ceb9ccec4b07
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-20 15:22:34 +0200

    lavoro su Read dei dati

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -7,7 +7,36 @@
             <h1>Lista posts</h1>
         </div>
         <div class="card-body">
-            
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Slug</th>
+                        <th scope="col">Content</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Modify</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>{{$post->id}}</td>
+                            <td>{{$post->title}}</td>
+                            <td>{{$post->slug}}</td>
+                            <td>{{substr($post->content, 0, 10)}}</td>  
+                            <td>
+                                @if ($post->published)
+                                    <span class="badge bg-success">Published</span>
+                                @else
+                                <span class="badge bg-warning text-dark">Not Published</span>
+                                @endif
+                            </td>
+                            <td>azioni per dopo</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+              </table>
         </div>   
     </div>
 </div>         
