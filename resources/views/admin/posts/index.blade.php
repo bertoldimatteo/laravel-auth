54422741e22644dd312444f8119c6c6b716c9cab
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-20 14:53:13 +0200

    aggiunta sezione Read della CRUD

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h1>Lista posts</h1>
+        </div>
+        <div class="card-body">
+            
+        </div>   
+    </div>
+</div>         
+@endsection
